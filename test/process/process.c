1dc3d0c55ea15f40be51c132a7273c6dcd741d15
mitsu1119 <hachimitsu1119@gmail.com>
2019-09-20 21:39:17 +0900

    I forgot to use if-block.

--- test/process/process.c
+++ test/process/process.c
@@ -7,19 +7,17 @@ int main() {
 
 	pid = fork();
 
-	if(pid == 0) printf(">>>>> This is child process [%d]\n\n", getpid());
-	if(pid == 0) printf("Print str [%.*s]\n\n", 3, str);
-	if(pid == 0) printf("Do command 'ls'\n");
-	if(pid == 0) execl("/bin/ls", "ls", 0);
-
-	if(pid == 0) buf = 10;
-	else printf(">>>>> This is parent process [%d]\n\n", getpid());
-	if(pid == 0) buf = 10;
-	else printf("Print str [%.*s]\n\n", 3, str);
-	if(pid == 0) buf = 10;
-	else waitpid(pid, &status, 0);
-	if(pid == 0) buf = 10;
-	else printf("\nChild process [%d]... end\n", pid);
+	if(pid == 0) {
+		printf(">>>>> This is child process [%d]\n\n", getpid());
+		printf("Print str [%.*s]\n\n", 3, str);
+		printf("Do command 'ls'\n");
+		execl("/bin/ls", "ls", 0);
+	} else {
+		printf(">>>>> This is parent process [%d]\n\n", getpid());
+		printf("Print str [%.*s]\n\n", 3, str);
+		waitpid(pid, &status, 0);
+		printf("\nChild process [%d]... end\n", pid);
+	}	
 
 	return 0;
 }
