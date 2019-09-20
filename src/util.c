15153892c9fbc46c4fd4b9701d10b6973480f541
mitsu1119 <hachimitsu1119@gmail.com>
2019-09-20 16:18:46 +0900

    Power up error message.

--- src/util.c
+++ src/util.c
@@ -5,11 +5,23 @@ void error(char *loc, char *fmt, ...) {
 	va_start(ap, fmt);
 
 	if(loc) {
-		int pos = loc - userInput;
+		char *line = loc;
+		while(userInput < line && line[-1] != '\n') line--;
 
-		fprintf(stderr, "%s\n", userInput);
-		for(; pos > 0; pos--) fprintf(stderr, " ");
-		fprintf(stderr, "^\n");
+		char *end = loc;
+		while(*end != '\n') end++;
+		
+		int errorLine = 1;
+		for(char *s = userInput; s < line; s++) {
+			if(*s == '\n') errorLine++;
+		}
+
+		int indent = fprintf(stderr, "%d: ", errorLine);
+		fprintf(stderr, "%.*s\n", (int)(end - line), line);
+
+		int pos = loc - line + indent;
+		fprintf(stderr, "%*s", pos, " ");
+		fprintf(stderr, "^ ");
 		vfprintf(stderr, fmt, ap);
 		fprintf(stderr, "\n");
 	} else {
