da3207edbf07e0b7cc9a40acb61e8037603d77ef
mitsu1119 <hachimitsu1119@gmail.com>
2019-09-22 13:15:34 +0900

    Delet a test program.

--- test/ptrCal/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-../../bin/run ptrcal.c nodebug > ../../dst/ptrcal.s
-gcc -c ../../dst/ptrcal.s -o ../../dst/ptrcal.o
-gcc -c allocate.c -o ../../dst/allocate.o
-gcc -o ../../bin/ptrcal ../../dst/allocate.o ../../dst/ptrcal.o
-../../bin/ptrcal
-echo $?
